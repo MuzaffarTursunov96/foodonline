260d1552d95d89e5107ba7da13373c3227e61d7c
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-10-18 13:44:02 +0300

    third commit

---
+++ accounts/utils.py
@@ -0,0 +1,9 @@
+
+def detectUser(user):
+  if user.role == 1:
+    redirectUrl ='vendorDashboard'
+  elif user.role == 2:
+    redirectUrl ='customerDashboard'
+  elif user.role == None and user.is_superadmin:
+    redirectUrl ='/admin'
+  return redirectUrl
